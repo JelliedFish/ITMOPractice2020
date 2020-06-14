a54025aeef2487d22b5f60d6777ca5c6a4bc5b43
JelliedFish <hungrydog757@gmail.com>
2020-06-14 16:24:51 +0300

    Initial commit

---
+++ lib/services/Catalog.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MathCatalog extends StatelessWidget{
+
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Каталог Математики"
+        ),
+      ),
+
+    );
+  }
+}
+
+
+class RussianCatalog extends StatelessWidget{
+
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Каталог Русского"
+        ),
+      ),
+
+    );
+  }
+}
