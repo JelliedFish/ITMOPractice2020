be9d569dcb3c95d85747465fdf18f0a3195220f2
Bey Sviatoslav Igorevich <bey.sviatoslav@yandex.ru>
2020-06-24 02:44:04 +0300

    implementing all except timer and answer button

--- lib/Test/view/TestView.dart
+++ lib/Test/view/TestView.dart
@@ -2,9 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutterapp/Test/presenter/TestPresenter.dart';
+import 'package:flutterapp/assets/ege_helper_icons.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class TestView extends StatelessWidget {
   var _testPresenter;
+  PanelController _pc = new PanelController();
 
   TestView(TestPresenter testPresenter) {
     _testPresenter = testPresenter;
@@ -29,6 +32,18 @@ class TestView extends StatelessWidget {
                     },
                   ),
                   padding: EdgeInsets.only(left: 5),
+                ),
+                Container(
+                  child: IconButton(
+                    color: _testPresenter
+                        .mainPresenter.mainPresenterModel.themeColor,
+                    icon: Icon(EgeHelper.logo),
+                    iconSize: 30,
+                    onPressed: () {
+                      this._testPresenter.goBack(context);
+                    },
+                  ),
+                  padding: EdgeInsets.only(left: 5),
                 )
               ],
             ),
@@ -36,16 +51,21 @@ class TestView extends StatelessWidget {
     );
   }
 
-  Swiper createSwipeLayout(){
+  Swiper createSwipeLayout() {
     return new Swiper(
       itemBuilder: (BuildContext context, int index) {
-        return new Card(
-          child: Container(
-            child: Text(
-              'Задание ${index+1}'
-            ),
-            width: 250,
-            height: 400,
+        return new Container(
+          child: Column(
+            children: <Widget>[
+              new Expanded(
+                child: createTaskCard(),
+                flex: 7,
+              ),
+              new Expanded(
+                child: createAnswerCard(),
+                flex: 2,
+              ),
+            ],
           ),
         );
       },
@@ -55,22 +75,119 @@ class TestView extends StatelessWidget {
     );
   }
 
+  Card createTaskCard() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Text(
+              'На диаграмме показан средний балл участников 10 стран в тестировании учащихся 4-го класса, по естествознанию в 2007 году (по 1000-балльной шкале). По данным диаграммы найдите число стран, в которых средний балл участников выше, чем в Венгрии.',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+          ),
+          Container(
+              child: const Image(
+                  image:
+                      NetworkImage('https://ege.sdamgia.ru/get_file?id=37577')),
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0))
+        ],
+      ),
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20),
+    );
+  }
+
+  Widget createAnswerCard() {
+    return Card(
+      child: new Row(children: <Widget>[
+        new Expanded(
+          child: IconButton(
+            onPressed: () {
+            },
+            icon: Icon(
+              EgeHelper.pencil,
+              color: _testPresenter.mainPresenter.mainPresenterModel.themeColor,
+            ),
+          ),
+          flex: 2,
+        ),
+        new Expanded(
+          child: Container(
+            child: Center(
+                child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Ответ',
+                  hintText: 'Введите ответ',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0))),
+            )),
+          ),
+          flex: 8,
+        ),
+        new Expanded(
+          child: IconButton(
+            onPressed: () {
+              _pc.open();
+            },
+            icon: Icon(
+              EgeHelper.question_circle_o,
+              color: _testPresenter.mainPresenter.mainPresenterModel.themeColor,
+            ),
+          ),
+          flex: 2,
+        )
+      ]),
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10),
+    );
+  }
+
+  Widget _floatingPanel(){
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20.0,
+              color: Colors.grey,
+            ),
+          ]
+      ),
+      child: Center(
+        child: Text("This is the SlidingUpPanel when open"),
+      ),
+    );
+  }
+
+  Widget createTest(BuildContext context){
+    return SlidingUpPanel(
+      controller: _pc,
+      body: Column(children: <Widget>[
+        new Expanded(
+          child: createFirstElement(context),
+          flex: 1,
+        ),
+        new Expanded(
+          child: Container(
+            child: Center(
+              child: createSwipeLayout(),
+            ),
+          ),
+          flex: 9,
+        ),
+      ]
+      ),
+      panel: _floatingPanel(),
+      minHeight: 0,
+    );
+  }
   @override
   Widget build(BuildContext context) {
+    Widget test = createTest(context);
     return Scaffold(
-        body: Column(children: <Widget>[
-          new Expanded(
-            child: createFirstElement(context),
-            flex: 1,
-          ),
-          new Expanded(
-            child: Container(
-              child: Center(
-                child: createSwipeLayout(),
-              ),
-            ),
-            flex: 9,
-          ),
-    ]));
+        body: test,
+    );
   }
 }
